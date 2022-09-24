d19952d462f04876e10eea2f9926001ee9dabf01
pubggamerzytmmm <113839144+pubggamerzytmmm@users.noreply.github.com>
2022-09-24 11:52:20 +0530

    Update values.js

--- values.js
+++ values.js
@@ -1,5 +1,5 @@
 exports.VAL = {
-    token:"752478208:AAH4k4LqQcuFfzlNG156xiB4fTQQ9GRPqog",
+    token:"5549679088:AAH6eakaCjNBrH1WOKBf9KOd9xFiJzSzo4I",
 
     apiKeys : ["72e27f5bf810febc87850ade6f075ffeb3afe593", "dea1ad4dad69807b36ca1e42d6af99c9be352fd5", "5f09e6a0cfd938f2d6734bc01e3ce6d1c3658444", "7eb059db605d7a2a02315bb5a339deade438f8f3"],
 
